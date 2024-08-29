0669b41f8f04ec8076b706c601f9445bdf88b85b
fabriciolopesx <fabricio.lopesx@gmail.com>
2024-08-29 16:01:16 -0300

    add bootstrap

---
+++ src/components/FirstNavigation.vue
@@ -0,0 +1,23 @@
+<template>
+  <nav class="navbar navbar-light bg-light">
+    <div class="container">
+      <a class="navbar-brand" href="#">
+        <img src="@/assets/logo.svg" width="70" alt="" />
+      </a>
+      <div class="socials">
+        <AwesomeSocialButton type="facebook" :link="{ src: 'https://facebook.com' }" />
+        <AwesomeSocialButton type="linkedin" :link="{ src: 'https://linkedin.com' }" />
+      </div>
+    </div>
+  </nav>
+</template>
+
+<script setup>
+import { AwesomeSocialButton } from 'awesome-social-button'
+</script>
+<style scoped>
+.awesome-social-button {
+  display: inline-block;
+  margin: 0 5px;
+}
+</style>
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,4 @@
-import './assets/main.css'
-
+import 'bootstrap/dist/css/bootstrap.min.css'
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
