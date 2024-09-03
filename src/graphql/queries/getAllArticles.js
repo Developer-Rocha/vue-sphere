0d4bf75373a00d7b22c38d8deb471495e99abb41
fabriciolopesx <fabricio.lopesx@gmail.com>
2024-09-03 07:30:02 -0300

    adjust getAllArticles query sort

--- src/graphql/queries/getAllArticles.js
+++ src/graphql/queries/getAllArticles.js
@@ -3,7 +3,10 @@ import { ARTICLE_FIELDS } from '../fragments/articleFragments';
 
 export const GET_ALL_ARTICLES = gql`
   query GetAllArticles {
-    nodeQuery(filter: { conditions: [{ field: "status", value: "1" }] }) {
+    nodeQuery(
+      filter: { conditions: [{ field: "status", value: "1" }] }
+      sort: {field: "created" direction: DESC}
+    ){
       entities {
         ...ArticleFields
       }
