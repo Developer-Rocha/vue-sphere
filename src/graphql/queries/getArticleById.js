0da675a7a720e631b91052d64e93dc8d345e29cb
fabriciolopesx <fabricio.lopesx@gmail.com>
2024-09-04 15:09:05 -0300

    improve the getArticleById query code

--- src/graphql/queries/getArticleById.js
+++ src/graphql/queries/getArticleById.js
@@ -1,48 +1,11 @@
 import { gql } from 'graphql-tag'
+import { ARTICLE_FIELDS } from '../fragments/articleFragments'
 
 export const GET_ARTICLE_BY_ID = gql`
   query GetArticleById($nid: String, $language: LanguageId) {
     nodeById(id: $nid, language: $language) {
-      ... on NodeArticle {
-        title
-        nid
-        created
-        entityOwner {
-          name
-        }
-        body {
-          value
-          format
-          processed
-        }
-        fieldTeaser
-        fieldImage {
-          entity {
-            ... on MediaImage {
-              fieldMediaImage {
-                url
-              }
-            }
-          }
-        }
-        fieldTags {
-          targetId
-          entity {
-            entityId
-            tid
-            name
-            entityBundle
-            weight
-            ... on TaxonomyTermTags {
-              description {
-                value
-                format
-                processed
-              }
-            }
-          }
-        }
-      }
+      ...ArticleFields
     }
   }
+  ${ARTICLE_FIELDS}
 `
