387ad5f383614b40925384e0ac0a9e700b1331ff
fabriciolopesx <fabricio.lopesx@gmail.com>
2024-09-05 08:57:19 -0300

    add toggleMenu

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,36 @@
 <script setup>
+import { ref } from 'vue'
 import { RouterView } from 'vue-router'
+import { IconMenu2 } from '@tabler/icons-vue'
 // components
 import MainNavigation from './components/MainNavigation.vue'
 import SocialNavigation from './components/SocialNavigation.vue'
 import SwitchLanguage from './components/SwitchLanguage.vue'
+
+const showMenu = ref(false)
+
+function toggleMenu() {
+  showMenu.value = !showMenu.value
+}
 </script>
 
 <template>
-  <header class="tm-header" id="tm-header">
+  <header :class="[showMenu ? 'show' : '', 'tm-header']" id="tm-header">
     <div class="tm-header-wrapper">
-      <button class="navbar-toggler" type="button" aria-label="Toggle navigation">
-        <i class="fas fa-bars"></i>
+      <button
+        class="navbar-toggler"
+        type="button"
+        aria-label="Toggle navigation"
+        @click="toggleMenu()"
+      >
+        <IconMenu2 size="25" />
       </button>
       <div class="tm-site-header">
         <div class="mb-3 mx-auto tm-site-logo"><i class="fas fa-times fa-2x"></i></div>
         <h1 class="text-center">Vue Sphere</h1>
       </div>
-      <SwitchLanguage />
       <MainNavigation />
+      <SwitchLanguage />
       <SocialNavigation />
     </div>
   </header>
