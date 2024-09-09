3d65602333180ae868f71cfb3b87d613e27a457d
fabriciolopesx <fabricio.lopesx@gmail.com>
2024-09-09 10:27:44 -0300

    clean code

--- src/apollo/client.js
+++ src/apollo/client.js
@@ -1,6 +1,5 @@
 import { ApolloClient, createHttpLink } from '@apollo/client/core'
 import cache from './cache'
-import { setContext } from '@apollo/client/link/context'
 
 const httpLink = createHttpLink({
   uri: import.meta.env.VITE_GRAPHQL_ENDPOINT,
@@ -8,17 +7,6 @@ const httpLink = createHttpLink({
     Authorization: `Bearer ${import.meta.env.VITE_GRAPHQL_TOKEN}`
   }
 })
-
-// Adiciona o token de autenticação, se necessário
-// const authLink = setContext((_, { headers }) => {
-//   const token = localStorage.getItem('token');
-//   return {
-//     headers: {
-//       ...headers,
-//       authorization: token ? `Bearer ${token}` : "",
-//     }
-//   }
-// });
 
 const apolloClient = new ApolloClient({
   link: httpLink,
